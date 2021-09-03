0abe54e70ea0a4ab534fd3821905a21396045c20
BazanAlexandro <alexander-bazan@outlook.com>
2021-09-03 19:46:05 +0300

    banner implementation

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,11 @@
 const path = require('path')
 const ESLintPlugin = require('eslint-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const { format } = require('date-fns')
+const pkg = require('./package.json')
+
+const now = new Date()
 
 module.exports = {
 	mode: 'development',
@@ -27,6 +32,15 @@ module.exports = {
 		new ESLintPlugin(),
 		new MiniCssExtractPlugin({
 			filename: 'out/Mix.css'
+		}),
+		new webpack.BannerPlugin({
+			banner: `
+				/*!
+				  Mix.js - v${pkg.version},
+				  ${format(now, 'yyyy-MM-dd')},
+				  Copyright (c) ${format(now, 'yyyy')} ${pkg.author},
+				*/
+			`,
 		})
 	],
 }
